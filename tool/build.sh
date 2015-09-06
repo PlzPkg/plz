18ad02e1a33363bbee4d31c611b40960e23048c5
Alec Joiner <joineral2@gmail.com>
2015-09-06 12:04:28 -0400

    Attempting to fix coverage reporting part 2

--- tool/build.sh
+++ tool/build.sh
@@ -6,7 +6,7 @@ if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
     dart --observe=2000 test/*.dart &
     pub global activate coverage
     pub global run coverage:collect_coverage --port=2000 -o report/coverage.json --resume-isolates
-    pub run dart_coveralls upload --exclude-test-files report
+    pub run dart_coveralls upload --token $COVERALLS_TOKEN --exclude-test-files report
 else
     pub run test
 fi
