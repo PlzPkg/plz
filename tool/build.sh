5b5badb3f2b45045ac1f95e503e4c0e0ec1decac
Alec Joiner <joineral2@gmail.com>
2015-09-06 12:00:07 -0400

    Attempting to fix coverage reporting

--- tool/build.sh
+++ tool/build.sh
@@ -2,9 +2,13 @@
 set -e
 
 dartanalyzer lib/*.dart bin/*.dart
-pub run test
 if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
-    pub run dart_coveralls report --exclude-test-files test/*.dart
+    dart --observe=2000 test/*.dart &
+    pub global activate coverage
+    pub global run coverage:collect_coverage --port=2000 -o report/coverage.json --resume-isolates
+    pub run dart_coveralls upload --exclude-test-files report
+else
+    pub run test
 fi
 dart2js --categories=Server bin/plz.dart
 cat $DART_SDK/lib/_internal/js_runtime/lib/preambles/d8.js out.js >> node.js
