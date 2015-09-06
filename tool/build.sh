16fbc25b05623cb60da4e4dfd6ed8525de1ad207
Alec Joiner <joineral2@gmail.com>
2015-09-06 12:22:07 -0400

    dart_coveralls doesn't support multiple test files

--- tool/build.sh
+++ tool/build.sh
@@ -4,7 +4,7 @@ set -e
 dartanalyzer lib/*.dart bin/*.dart
 pub run test
 if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
-    pub run dart_coveralls report --exclude-test-files test/*.dart
+    pub run dart_coveralls report --exclude-test-files test/plz_lib_test.dart
 fi
 dart2js --categories=Server bin/plz.dart
 cat $DART_SDK/lib/_internal/js_runtime/lib/preambles/d8.js out.js >> node.js
